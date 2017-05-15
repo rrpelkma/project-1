9e68bb13bcb7bfa3ceecb2c12d87aba5fb88e643
Ramon Pelkmans <rrpelkma@avans.nl>
2017-05-15 13:49:26 +0200

    k

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/webhook/', function (req, res) {
 			let text = event.message.text
 			if (text === 'Generic'){ 
 				console.log("welcome to chatbot")
-				//sendGenericMessage(sender)
+				sendGenericMessage(sender)
 				continue
 			}
 			if(text.includes("kaart")){ 
