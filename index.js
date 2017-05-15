a17992eb888b8e82f6712f892d23f48f42b2d85e
Ramon Pelkmans <rrpelkma@avans.nl>
2017-05-15 23:07:42 +0200

    test

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ function sendGenericMessage(sender) {
 						"title": "Postback",
 						"payload": "Payload for first element in a generic bubble",
 					}],
-				}, {
+				}, /*{
 					"title": "Second card",
 					"subtitle": "Element #2 of an hscroll",
 					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
@@ -129,7 +129,7 @@ function sendGenericMessage(sender) {
 						"title": "Postback",
 						"payload": "Payload for second element in a generic bubble",
 					}],
-				}]
+				}]*/
 			}
 		}
 	}
